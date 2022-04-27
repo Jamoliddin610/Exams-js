e26bf05ca51e82c066326afe977b3b9751a321a2
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-04-27 11:36:05 +0500

    jamoliddin

--- js/beshinchi-masala.js
+++ js/beshinchi-masala.js
@@ -1,1 +1,11 @@
-let 
+let obj = { x: 1, y: 2 };
+
+function arr(obj) {
+    let arr = [];
+    for (let i in obj) {
+        let num = [i, obj[i]];
+        arr.push(num);
+    }
+    console.log(arr);
+}
+arr(obj);
